024c477b6f290fda7940bfc37f435d4f74474c22
Quinten Tupker <qmt20@cam.ac.uk>
2018-08-25 11:13:00 -0400

    Complted AUCRegress21To28

--- IterativeAlgo/massTest.m
+++ IterativeAlgo/massTest.m
@@ -6,7 +6,7 @@ sigmas = -2:0.2:2;
 noises = 0:10;
 
 googleSpreadSheetID = '1mBe8fQp-5RCBkfMJ35gHHYcZTxHu5e9TeM7xo15F85o';
-googleSheetID = '1200493859';
+googleSheetID = '120197944';
 
 numRegions = 6;
 numNoises = length(noises);
